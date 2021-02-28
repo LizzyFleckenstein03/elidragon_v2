4ebfbedb226654376b033fc2f40583ef9f18fd17
Elias Fleckenstein <eliasfleckenstein@web.de>
2021-02-28 12:10:10 +0100

    Make update script actually work

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,7 +3,12 @@
 # Update all software used by the server
 
 # Pull repository
-git pull --recurse-submodules
+git pull
+
+# Update submodules
+git submodule init
+git submodule update
+git submodule foreach "git fetch origin; git checkout $(git rev-parse --abbrev-ref HEAD); git reset --hard origin/$(git rev-parse --abbrev-ref HEAD); git submodule update --recursive; git clean -dfx"
 
 # Update minetest and screen
 sudo apt-get update -y
