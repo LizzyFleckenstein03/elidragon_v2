4fbdc312ae2635bed2551f432a69184d7ba3d472
Elias Fleckenstein <eliasfleckenstein@web.de>
2021-03-13 20:39:56 +0100

    Separate elidragon_worlds mod

--- mods/elidragon_worlds/init.lua
+++ mods/elidragon_worlds/init.lua
@@ -5,6 +5,7 @@ for _, world in ipairs(elidragon.WORLDS) do
 	if world ~= current_world then
 		local desc = world:sub(1, 1):upper() .. world:sub(2)
 		descriptions[world] = desc
+
 		minetest.register_chatcommand(world, {
 			description = "Join " .. desc,
 			func = function(name)
@@ -16,6 +17,8 @@ end
 
 multiserver.register_on_redirect_done(function(name, world, success)
 	if not success then
-		minetest.send_chat_message(name, minetest.colorize(mcl_colors.RED, descriptions[world] .. " is down."))
+		minetest.chat_send_player(name, minetest.colorize(mcl_colors.RED, descriptions[world] .. " is down."))
 	end
 end)
+
+elidragon.worlds = {}
